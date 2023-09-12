48fff27e4ffc7bdf842e70a56345593222c4219d
Amro Amin <a.amin@ps-tt.com>
2023-09-12 10:09:59 +0300

    Initial Commit

---
+++ assets/scripts.js
@@ -0,0 +1,6 @@
+// const myCarouselElement = document.querySelector('#availableServicesCarousel')
+
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+//   interval: 0,
+//   touch: false
+// })
